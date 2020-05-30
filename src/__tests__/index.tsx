d58266569708162c2d947bb49941dc7c9858996b
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2020-05-30 01:38:14 -0400

    snapshot testing (#6)
    * snapshot testing
    
    * With Read me

--- src/__tests__/index.tsx
+++ src/__tests__/index.tsx
@@ -1,10 +1,18 @@
-import { shallow } from "enzyme";
-import * as React from 'react'
+import React from 'react'
+import { shallow } from "enzyme"
 import ReactFancyNavigation from '../index'
+import renderer from "react-test-renderer";
 
 describe('React Navigation test', () => {
   it('Renders', async () => {
     const component = shallow(<ReactFancyNavigation animate="none" menuType="hamburger" title="Heading" />).contains(<header>Pre Header</header>)
     expect(component).toBeTruthy()
+  })
+
+  it('does snapshot testing', () => {
+    const tree = renderer
+      .create(<ReactFancyNavigation animate="none" menuType="hamburger" title="Heading" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot()
   })
 })
