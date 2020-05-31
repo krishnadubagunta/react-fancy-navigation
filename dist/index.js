9625127fa92aebdd64eb1f32fcce460179f49aea
Sai Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2020-05-31 13:34:16 -0400

    tests

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 var React = _interopDefault(require('react'));
 
 var ReactFancyNavigation = function ReactFancyNavigation(props) {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("header", null, "Div Header"), /*#__PURE__*/React.createElement("pre", {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("header", null, "Div Header 904"), /*#__PURE__*/React.createElement("pre", {
     dangerouslySetInnerHTML: {
       __html: JSON.stringify(props, undefined, 2)
     }
