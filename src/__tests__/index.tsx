a1f9dacdfca290d09f03bc31ebd29449402eab12
Sai Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2020-05-31 17:25:12 -0400

    test passes

--- src/__tests__/index.tsx
+++ src/__tests__/index.tsx
@@ -1,8 +1,19 @@
-describe('React Navigation test', () => {
-  it('visual regression testing', async () => {
-    await page.goto('http://localhost:3000')
-    const image = await page.screenshot()
+import React from 'react'
+import { shallow } from "enzyme"
+import ReactFancyNavigation from '../index'
+import renderer from "react-test-renderer";
 
-    expect(image).toMatchImageSnapshot()
+describe('React Navigation test', () => {
+  it('Renders', async () => {
+    const component = shallow(<ReactFancyNavigation animate="none" menuType="hamburger" title="Heading" />).contains(<header>Div Header 904</header>)
+    debugger
+    expect(component).toBeTruthy()
+  })
+
+  it('does snapshot testing', () => {
+    const tree = renderer
+      .create(<ReactFancyNavigation animate="none" menuType="hamburger" title="Heading" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot()
   })
 })
